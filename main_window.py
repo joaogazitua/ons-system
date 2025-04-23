84ab88eecc59a88bc0e90518eac651ffd72ed51c
Joao Victor Evaristo de Albuquerque Gazitua <joaovictor.evaristodealbuquerquegazitua@stud.h-da.de>
2025-04-23 21:27:14 +0200

    setup da ui realizado. Algumas funcionalidades adicionadas, como os blocos de input (mas precisam de conserto)

---
+++ main_window.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class MainWindow():
+    def __init__(self, root):
+        self.dev_team_value = None
+        self.fixed_alocation = None
+        self.percent_for_retrospective = None
+        self.percent_for_review = None
+        self.percent_for_planning = None
+        self.percent_for_tests = None
+        self.first_sprint_date = None
+        self.days_per_sprint = None
+        self.total_sprint_dev_hours = None
+        self.total_dev_hours = None
+        self.root = root
+        self.root.title("Gerador de Tabelas de Planejamento")
+        self.root.geometry("800x600")
+        self.setup_ui()
+
+    def setup_ui(self):
+        main_frame= ttk.Frame(self.root, padding=20)
+        main_frame.pack(fill=tk.BOTH, expand=True)
+
+        ttk.Label(main_frame, text="Quantidade de horas totais de desenvolvimento do Projeto:").grid(row=0, column=0, sticky=tk.W, pady=5)
+        self.total_dev_hours = ttk.Entry(main_frame)
+        self.total_dev_hours.grid(row=0, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Quantidade de horas de desenvolvimento para cada Sprint:").grid(row=1, column=0, sticky=tk.W, pady=5)
+        self.total_sprint_dev_hours = ttk.Entry(main_frame)
+        self.total_sprint_dev_hours.grid(row=1, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Quantidade de dias por Sprint:").grid(row=2, column=0, sticky=tk.W,pady=5)
+        self.days_per_sprint = ttk.Entry(main_frame)
+        self.days_per_sprint.grid(row=2, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Data de inicio da primeira Sprint (dd/mm/yyyy):").grid(row=3, column=0, sticky=tk.W, pady=5)
+        self.first_sprint_date = ttk.Entry(main_frame)
+        self.first_sprint_date.grid(row=3, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Percentual para testes:").grid(row=4, column=0, sticky=tk.W,pady=5)
+        self.percent_for_tests = ttk.Entry(main_frame)
+        self.percent_for_tests.grid(row=4, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Percentual para Planning:").grid(row=5, column=0, sticky=tk.W, pady=5)
+        self.percent_for_planning = ttk.Entry(main_frame)
+        self.percent_for_planning.grid(row=5, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Percentual para Review:").grid(row=6, column=0, sticky=tk.W, pady=5)
+        self.percent_for_review = ttk.Entry(main_frame)
+        self.percent_for_tests.grid(row=6, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Percentual para Retrospectiva:").grid(row=7, column=0, sticky=tk.W, pady=5)
+        self.percent_for_retrospective = ttk.Entry(main_frame)
+        self.percent_for_tests.grid(row=7, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Alocacao fixa de horas:").grid(row=8, column=0, sticky=tk.W, pady=5)
+        self.fixed_alocation = ttk.Entry(main_frame)
+        self.fixed_alocation.grid(row=8, column=1, pady=5)
+
+        ttk.Label(main_frame, text="Valor por hora do time de desenvolvimento:").grid(row=9, column=0, sticky=tk.W, pady=5)
+        self.dev_team_value = ttk.Entry(main_frame)
+        self.dev_team_value.grid(row=9, column=1, pady=5)
+
+        genetate_table_btn = ttk.Button(main_frame, text="Gerar Tabela", command=self.generate_table)
+        genetate_table_btn.grid(row=20, column=2, columnspan=2, pady=20)
+
+
+    def generate_table(self):
+        project_dev_hours = self.total_dev_hours.get()
+        sprint_dev_hours = self.total_sprint_dev_hours.get()
+        print(f"Horas totais de desenvolvimento do Projeto: {project_dev_hours}")
+        print(f"Horas de desenvolvimento por Sprint: {sprint_dev_hours}")
